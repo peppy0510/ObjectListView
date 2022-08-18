7c52a092aa4c83daef509784200db2f28657323f
Taehong Kim <peppy0510@hotmail.com>
2022-08-18 23:51:04 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+# encoding: utf-8
+
+
+'''
+author: Taehong Kim
+email: peppy0510@hotmail.com
+'''
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='ObjectListView',
+    version='1.3.2',
+    packages=['ObjectListView'],
+    author='Taehong Kim',
+    author_email='peppy0510@hotmail.com',
+    description='',
+    long_description='',
+)
